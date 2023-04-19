b89fc9c2021e2c3d91e5347708052378e50ef6ca
pumchanpom <pum.chanpom@gmail.com>
2023-04-19 10:01:27 +0700

    Edit Navbar file.

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,9 +1,9 @@
-
+import { Link, Routes, Route } from 'react-router-dom';
 const Navbar = () => {
     return (
         <ul style={{listStyleType: "none", display: "flex", justifyContent: "flex-end", gap: "40px", fontSize: "20px", fontWeight: "700"}}>
-            <li><a href={'/'}>Home</a></li>
-            <li><a href={'/Owner'}>Owner</a></li>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/Owner">Owner</Link></li>
         </ul>
     )
 }
